be621e7591b0d134cfe0c5d57db4d257730cf1ea
unknown <kavinprathaban025@gmail.com>
2020-10-23 17:12:23 +0530

    backend initiated and fetched data from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = ({match}) => {
-    const product = products.find((p) => p._id === match.params.id)
+    
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${match.params.id}`);
+
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [match]);
 
     return (
         <div>
@@ -28,7 +38,7 @@ const ProductScreen = ({match}) => {
                             />
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            Price : ${product.price}
+                            Price : LKR{product.price}
                         </ListGroup.Item>
                         <ListGroup.Item>
                             Description : ${product.description}
@@ -44,7 +54,7 @@ const ProductScreen = ({match}) => {
                                         Price:
                                     </Col>
                                     <Col>
-                                        <strong>${product.price}</strong>
+                                        <strong>LKR{product.price}</strong>
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
